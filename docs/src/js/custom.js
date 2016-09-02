195c59ad6e1eba1dfec79b641c1b3ba797810ced
microcipcip <microcipcip@gmail.com>
2016-09-03 00:05:51 +0100

    Standarized spacing and improved gitter chat styling

--- docs/src/js/custom.js
+++ docs/src/js/custom.js
@@ -1,205 +1,205 @@
-(function() {
-	'use strict';
-
-	//===============================================================/
-	//  =vue resource
-	//===============================================================/
-
-	var baseUrl = location.protocol + '//' + location.host + location.pathname;
-	Vue.http.options.root = baseUrl;
+(function () {
+  'use strict'
 
 
 
-	//===============================================================/
-	//  =router page
-	//===============================================================/
+  //===============================================================/
+  //  =vue resource
+  //===============================================================/
 
-	var Page = Vue.extend({
-		template: '{{{content}}}',
-		data: function() {
-			return {
-				content: '',
-				timerRouteStart: null,
-				timerRouteReady: null
-			};
-		},
-		methods: {
-		    highlight: function($parent) {
-			    var $codeBlock = $parent.querySelectorAll('pre code');
-			    Array.prototype.forEach.call($codeBlock, function($el, i) {
-				    hljs.highlightBlock($el);
-			    });
-		    }
-		},
-		route: {
-			activate: function(transition) {
-				this.timerRouteStart = Date.now() | 0;
-				transition.next();
-			},
-			data: function(transition) {
-				// generate url
-				var pageUrl = transition.to.path === '/' ? 'intro' : transition.to.path;
-				pageUrl = baseUrl + 'docs/pages/'+pageUrl+'.html';
-
-				this.$http.get(pageUrl).then(function(response) {
-					// display route after a small delay
-					this.timerRouteReady = Date.now() | 0;
-					var timerDiff = 500 - (this.timerRouteReady - this.timerRouteStart);
-
-					setTimeout(function () {
-						// update content
-						this.content = response.body;
-
-						// show page
-						document.body.classList.add('route-transition');
-
-						Vue.nextTick(function () {
-							// generate source code dinamically
-							var $docs = this.$root.$els.wrap.querySelectorAll('.doc');
-							Array.prototype.forEach.call($docs, function($doc, i){
-								var $docGrid    = $doc.querySelector('.doc__grid'),
-									docGridCnt  = this.$root.escapeDocs($docGrid.innerHTML),
-									docSource   = $doc.querySelector('.doc__source code');
-
-								docSource.innerHTML = docGridCnt;
-							}.bind(this));
-
-							// clean up any other pre code
-							var $docCustomSource = this.$root.$els.wrap.querySelectorAll('.doc-custom__source code');
-							Array.prototype.forEach.call($docCustomSource, function($docSource, i){
-								var docSourceCnt = this.$root.escapeDocsCustom($docSource.innerHTML);
-
-								$docSource.innerHTML = docSourceCnt;
-							}.bind(this));
-
-							// code highlighter
-							this.highlight(this.$root.$els.wrap);
-
-							// enable codepen
-							var $docDemo = this.$root.$els.wrap.querySelectorAll('.doc-demo');
-							if ($docDemo.length) {
-								this.$root.injectScript({
-									$appendTo: 'body',
-									url: '//assets.codepen.io/assets/embed/ei.js'
-								});
-							}
-						}.bind(this));
-
-						transition.next();
-					}.bind(this), timerDiff);
-				}, function(response) {
-					// error callback
-					document.body.classList.add('route-transition');
-					this.content =
-						'<div class="typ -center -text-center -text-medium">' +
-							'<h1>Page not found</h1>' +
-							'<p>Ooops...something went terribly wrong...</p>' +
-							'<img src="docs/dist/img/general/404.png" alt="">' +
-						'</div>'
-					;
-					transition.next();
-				});
-			},
-			canReuse: function() {
-				document.body.classList.remove('route-transition');
-			}
-		},
-		ready : function() {}
-	});
+  var baseUrl = location.protocol + '//' + location.host + location.pathname
+  Vue.http.options.root = baseUrl
 
 
 
-	//===============================================================/
-	//  =init
-	//===============================================================/
+  //===============================================================/
+  //  =router page
+  //===============================================================/
 
-	var App = Vue.extend({
-		data: function() {
-			return {};
-		},
-		methods: {
-			escapeDocs: function(unsafe) {
-				return unsafe
-					.replace(/<div class="doc__box".*?<\/div>/g, '...')
-					.replace(/^\t\t/gm, '') // remove first two tabs for each line
-					.replace(/\t/g, '  ') // replace all other tabs with two spaces
-					.replace(/^\s+|\s+$/g, '')
-					.replace(/&/g, '&amp;')
-					.replace(/</g, '&lt;')
-					.replace(/>/g, '&gt;')
-					.replace(/"/g, '&quot;')
-					.replace(/'/g, '&#039;');
-			},
-			escapeDocsCustom: function(unsafe) {
-				return unsafe
-					.replace(/^\t\t\t\t/gm, '') // remove first four tabs for each line
-					.replace(/\t/gm, '  ') // replace all other tabs with two spaces
-					.replace(/^\s+|\s+$/g, '')
-					.replace(/&/g, '&amp;')
-					.replace(/</g, '&lt;')
-					.replace(/>/g, '&gt;')
-					.replace(/"/g, '&quot;')
-					.replace(/'/g, '&#039;');
-			},
-			injectScript: function(opts) {
-				if (!opts.$appendTo || !opts.url) return;
+  var Page = Vue.extend({
+    template: '{{{content}}}',
+    data: function () {
+      return {
+        content: '',
+        timerRouteStart: null,
+        timerRouteReady: null
+      }
+    },
+    methods: {
+      highlight: function ($parent) {
+        var $codeBlock = $parent.querySelectorAll('pre code')
+        Array.prototype.forEach.call($codeBlock, function ($el, i) {
+          hljs.highlightBlock($el)
+        })
+      }
+    },
+    route: {
+      activate: function (transition) {
+        this.timerRouteStart = Date.now() | 0
+        transition.next()
+      },
+      data: function (transition) {
+        // generate url
+        var pageUrl = transition.to.path === '/' ? 'intro' : transition.to.path
+        pageUrl = baseUrl + 'docs/pages/' + pageUrl + '.html'
 
-				var $script = document.createElement('script');
-				$script.type = 'text/javascript';
-				$script.async = true;
-				$script.onload = function() {
-					$script.parentNode.removeChild($script);
-				};
-				$script.src = opts.url;
-				document.querySelector(opts.$appendTo).appendChild($script);
-			},
-			toggleNav: function(e) {
-				this.$els.header.classList.toggle('-expanded');
-			},
-			delegations: function(e) {
-				
-				//===============================================================/
-				//  =links 
-				//===============================================================/
+        this.$http.get(pageUrl).then(function (response) {
+          // display route after a small delay
+          this.timerRouteReady = Date.now() | 0
+          var timerDiff = 500 - (this.timerRouteReady - this.timerRouteStart)
 
-				var $link = e.target.closest('a');
-				if (!$link) return;
+          setTimeout(function () {
+            // update content
+            this.content = response.body
 
-				// route link
-				var dataHref = $link.dataset.href;
-				if (dataHref) {
-					e.preventDefault();
-					this.$router.go(dataHref);
-					return;
-				}
+            // show page
+            document.body.classList.add('route-transition')
 
-				 // toggle code
-				 var $sourceCode = this.$els.wrap.querySelector('[source="'+$link.getAttribute('source-target')+'"]');
-				 if ($sourceCode) {
-					 e.preventDefault();
-					 $link.classList.toggle('-expand');
-					 $sourceCode.classList.toggle('-expand');
-					 return;
-				 }
+            Vue.nextTick(function () {
+              // generate source code dinamically
+              var $docs = this.$root.$els.wrap.querySelectorAll('.doc')
+              Array.prototype.forEach.call($docs, function ($doc, i) {
+                var $docGrid = $doc.querySelector('.doc__grid'),
+                  docGridCnt = this.$root.escapeDocs($docGrid.innerHTML),
+                  docSource = $doc.querySelector('.doc__source code')
 
-			}
-		}
-	});
+                docSource.innerHTML = docGridCnt
+              }.bind(this))
 
-	var router = new VueRouter({
-		linkActiveClass: '-active',
-		transitionOnLoad: true,
-		hashbang: true
-	});
+              // clean up any other pre code
+              var $docCustomSource = this.$root.$els.wrap.querySelectorAll('.doc-custom__source code')
+              Array.prototype.forEach.call($docCustomSource, function ($docSource, i) {
+                var docSourceCnt = this.$root.escapeDocsCustom($docSource.innerHTML)
 
-	router.map({
-		'/': {
-			component : Page
-		},
-		':page': {
-			component : Page
-		}
-	});
+                $docSource.innerHTML = docSourceCnt
+              }.bind(this))
 
-	router.start(App, '#app');
-})();
+              // code highlighter
+              this.highlight(this.$root.$els.wrap)
+
+              // enable codepen
+              var $docDemo = this.$root.$els.wrap.querySelectorAll('.doc-demo')
+              if ($docDemo.length) {
+                this.$root.injectScript({
+                  $appendTo: 'body',
+                  url: '//assets.codepen.io/assets/embed/ei.js'
+                })
+              }
+            }.bind(this))
+
+            transition.next()
+          }.bind(this), timerDiff)
+        }, function (response) {
+          // error callback
+          document.body.classList.add('route-transition')
+          this.content =
+            '<div class="typ -center -text-center -text-medium">' +
+            '<h1>Page not found</h1>' +
+            '<p>Ooops...something went terribly wrong...</p>' +
+            '<img src="docs/dist/img/general/404.png" alt="">' +
+            '</div>'
+
+          transition.next()
+        })
+      },
+      canReuse: function () {
+        document.body.classList.remove('route-transition')
+      }
+    },
+    ready: function () {}
+  })
+
+
+
+  //===============================================================/
+  //  =init
+  //===============================================================/
+
+  var App = Vue.extend({
+    data: function () {
+      return {}
+    },
+    methods: {
+      escapeDocs: function (unsafe) {
+        return unsafe
+          .replace(/<div class="doc__box".*?<\/div>/g, '...')
+          .replace(/^    /gm, '') // remove first two tabs for each line
+          .replace(/^\s+|\s+$/g, '')
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#039;')
+      },
+      escapeDocsCustom: function (unsafe) {
+        return unsafe
+          .replace(/^        /gm, '') // remove first four tabs for each line
+          .replace(/^\s+|\s+$/g, '')
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#039;')
+      },
+      injectScript: function (opts) {
+        if (!opts.$appendTo || !opts.url) return
+
+        var $script = document.createElement('script')
+        $script.type = 'text/javascript'
+        $script.async = true
+        $script.onload = function () {
+          $script.parentNode.removeChild($script)
+        }
+        $script.src = opts.url
+        document.querySelector(opts.$appendTo).appendChild($script)
+      },
+      toggleNav: function (e) {
+        this.$els.header.classList.toggle('-expanded')
+      },
+      delegations: function (e) {
+
+        //===============================================================/
+        //  =links
+        //===============================================================/
+
+        var $link = e.target.closest('a')
+        if (!$link) return
+
+        // route link
+        var dataHref = $link.dataset.href
+        if (dataHref) {
+          e.preventDefault()
+          this.$router.go(dataHref)
+          return
+        }
+
+        // toggle code
+        var $sourceCode = this.$els.wrap.querySelector('[source="' + $link.getAttribute('source-target') + '"]')
+        if ($sourceCode) {
+          e.preventDefault()
+          $link.classList.toggle('-expand')
+          $sourceCode.classList.toggle('-expand')
+          return
+        }
+
+      }
+    }
+  })
+
+  var router = new VueRouter({
+    linkActiveClass: '-active',
+    transitionOnLoad: true,
+    hashbang: true
+  })
+
+  router.map({
+    '/': {
+      component: Page
+    },
+    ':page': {
+      component: Page
+    }
+  })
+
+  router.start(App, '#app')
+})()
--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,103 @@
 'use strict';
 
-
-
 //===============================================================/
 //  =initialization
 //===============================================================/
 
 // include plugins
-var gulp            = require('gulp'),
-	argv            = require('yargs').argv,
-	concat          = require('gulp-concat'),
-	changed         = require('gulp-changed'),
-	cssBase64       = require('gulp-css-base64'),
-	cssImgSize      = require("gulp-css-image-dimensions"),
-	del             = require('del'),
-	es6Promise      = require('es6-promise'), // required for autoprefixer
-	gulpif          = require('gulp-if'),
-	gutil           = require('gulp-util'), // this is used by 'del'
-	header          = require('gulp-header'),
-	jeditor         = require('gulp-json-editor'),
-	jshint          = require('gulp-jshint'),
-	minifyCSS       = require('gulp-clean-css'),
-	prefix          = require('gulp-autoprefixer'),
-	plumber         = require('gulp-plumber'),
-	rename          = require('gulp-rename'),
-	replace         = require('gulp-replace'),
-	sass            = require('gulp-sass'),
-	stylish         = require('jshint-stylish-ex'),
-	uglify          = require('gulp-uglify');
+var gulp = require('gulp'),
+  argv = require('yargs').argv,
+  concat = require('gulp-concat'),
+  changed = require('gulp-changed'),
+  cssBase64 = require('gulp-css-base64'),
+  cssImgSize = require("gulp-css-image-dimensions"),
+  del = require('del'),
+  es6Promise = require('es6-promise'), // required for autoprefixer
+  gulpif = require('gulp-if'),
+  gutil = require('gulp-util'), // this is used by 'del'
+  header = require('gulp-header'),
+  jeditor = require('gulp-json-editor'),
+  jshint = require('gulp-jshint'),
+  minifyCSS = require('gulp-clean-css'),
+  prefix = require('gulp-autoprefixer'),
+  plumber = require('gulp-plumber'),
+  rename = require('gulp-rename'),
+  replace = require('gulp-replace'),
+  sass = require('gulp-sass'),
+  stylish = require('jshint-stylish-ex'),
+  uglify = require('gulp-uglify');
 
 // set project details here
 var project = {
-		shortname: "Gridy", // do not add spaces or special chars (used on json)
-		name: "Gridy",
-		descr: "A Flexbox Gridy System",
-		version: "0.9.0",
-		url: "https://microcipcip.github.io/gridy/",
-		author: "http://www.jertix.org"
-	},
-	projectInfo = '/*!' + ' \n' +
-		'   *Name:          <%= pkg.name %>' + ' \n' +
-		'   *Description:   <%= pkg.descr %>' + ' \n' +
-		'   *Version:       <%= pkg.version %>' + ' \n' +
-		'   *Website:       <%= pkg.url %>' + ' \n' +
-		'*/' + ' \n';
+    shortname: "Gridy", // do not add spaces or special chars (used on json)
+    name: "Gridy",
+    descr: "A Flexbox Gridy System",
+    version: "0.9.0",
+    url: "https://microcipcip.github.io/gridy/",
+    author: "http://www.jertix.org"
+  },
+  projectInfo = '/*!' + ' \n' +
+    '   *Name:          <%= pkg.name %>' + ' \n' +
+    '   *Description:   <%= pkg.descr %>' + ' \n' +
+    '   *Version:       <%= pkg.version %>' + ' \n' +
+    '   *Website:       <%= pkg.url %>' + ' \n' +
+    '*/' + ' \n';
 
 // set variables for local/deployment/live
 var isDev = argv.type === 'dev',
-	isDeployment = argv.type === 'depl',
-	isLive = argv.type === 'live';
+  isDeployment = argv.type === 'depl',
+  isLive = argv.type === 'live';
 
 // configure the path of each task
 var gridPath = {
-		src: 'src/',
-		dist: 'dist/',
-		distFiles: ['dist/**/*'],
-		root: ''
-	},
-	docsPath = {
-		src: 'docs/src/',
-		dist: 'docs/dist/',
-		distFiles: ['docs/dist/**/*'],
-		root: ''
-	},
-	imgExt = '.{jpeg,jpg,png,gif,svg,cur,ico}',
-	fontExt = '.{eot,ttf,otf,woff,woff2,svg}',
-	audioExt = '.{wav,mp3}',
-	path = {
-		src: {
-			gridscss        : gridPath.src + '',
-			gridscssFiles   : gridPath.src + '**/*.scss',
-			gridcss         : gridPath.src + '',
-			gridcssFiles    : gridPath.src + '**/*.css',
-			scss            : docsPath.src + 'scss/',
-			scssFiles       : docsPath.src + 'scss/**/*.scss',
-			css             : docsPath.src + 'css/',
-			cssFiles        : docsPath.src + 'css/**/*.css',
-			font            : docsPath.src + 'fonts/',
-			fontFiles       : docsPath.src + 'fonts/**/*' + fontExt,
-			js              : docsPath.src + 'js/',
-			jsFiles         : docsPath.src + 'js/**/*.js',
-			img             : docsPath.src + 'img/',
-			imgFiles        : docsPath.src + 'img/**/*' + imgExt
-		},
-		dist: {
-			gridscss        : gridPath.dist + '',
-			gridscssFiles   : gridPath.dist + '**/*.scss',
-			gridcss         : gridPath.dist + '',
-			gridcssFiles    : gridPath.dist + '**/*.css',
-			css             : docsPath.dist + 'css/',
-			cssFiles        : docsPath.dist + 'css/**/*.css',
-			font            : docsPath.dist + 'fonts/',
-			fontFiles       : docsPath.dist + 'fonts/**/*' + fontExt,
-			js              : docsPath.dist + 'js/',
-			jsFiles         : docsPath.dist + 'js/**/*.js',
-			img             : docsPath.dist + 'img/',
-			imgFiles        : docsPath.dist + 'img/**/*' + imgExt
-		},
-		imgPath: '../img',
-		json: 'package.json'
-	};
+    src: 'src/',
+    dist: 'dist/',
+    distFiles: [ 'dist/**/*' ],
+    root: ''
+  },
+  docsPath = {
+    src: 'docs/src/',
+    dist: 'docs/dist/',
+    distFiles: [ 'docs/dist/**/*' ],
+    root: ''
+  },
+  imgExt = '.{jpeg,jpg,png,gif,svg,cur,ico}',
+  fontExt = '.{eot,ttf,otf,woff,woff2,svg}',
+  audioExt = '.{wav,mp3}',
+  path = {
+    src: {
+      gridscss: gridPath.src + '',
+      gridscssFiles: gridPath.src + '**/*.scss',
+      gridcss: gridPath.src + '',
+      gridcssFiles: gridPath.src + '**/*.css',
+      scss: docsPath.src + 'scss/',
+      scssFiles: docsPath.src + 'scss/**/*.scss',
+      css: docsPath.src + 'css/',
+      cssFiles: docsPath.src + 'css/**/*.css',
+      font: docsPath.src + 'fonts/',
+      fontFiles: docsPath.src + 'fonts/**/*' + fontExt,
+      js: docsPath.src + 'js/',
+      jsFiles: docsPath.src + 'js/**/*.js',
+      img: docsPath.src + 'img/',
+      imgFiles: docsPath.src + 'img/**/*' + imgExt
+    },
+    dist: {
+      gridscss: gridPath.dist + '',
+      gridscssFiles: gridPath.dist + '**/*.scss',
+      gridcss: gridPath.dist + '',
+      gridcssFiles: gridPath.dist + '**/*.css',
+      css: docsPath.dist + 'css/',
+      cssFiles: docsPath.dist + 'css/**/*.css',
+      font: docsPath.dist + 'fonts/',
+      fontFiles: docsPath.dist + 'fonts/**/*' + fontExt,
+      js: docsPath.dist + 'js/',
+      jsFiles: docsPath.dist + 'js/**/*.js',
+      img: docsPath.dist + 'img/',
+      imgFiles: docsPath.dist + 'img/**/*' + imgExt
+    },
+    imgPath: '../img',
+    json: 'package.json'
+  };
 
 
 
@@ -109,115 +107,115 @@ var gridPath = {
 
 // clean
 gulp.task('clean', function (cb) {
-	del(gridPath.distFiles, cb);
-	del(docsPath.distFiles, cb);
+  del(gridPath.distFiles, cb);
+  del(docsPath.distFiles, cb);
 });
 
 // sass and css grid
 gulp.task('cssGrid', function () {
-	gulp.src(path.src.gridscssFiles)
-		.pipe(plumber())
-		.pipe(sass({
-			sourceComments: null,
-			precision: 7
-		}))
-		.pipe(prefix({
-			browsers: ['last 2 versions', '> 1%'],
-			cascade: false
-		}))
-		.pipe(header(projectInfo, {pkg: project}))
-		.pipe(gulp.dest('dist'))
-		.pipe(minifyCSS())
-		.pipe(rename('gridy.min.css'))
-		.pipe(gulp.dest(gridPath.dist));
+  gulp.src(path.src.gridscssFiles)
+    .pipe(plumber())
+    .pipe(sass({
+      sourceComments: null,
+      precision: 7
+    }))
+    .pipe(prefix({
+      browsers: [ 'last 2 versions', '> 1%' ],
+      cascade: false
+    }))
+    .pipe(header(projectInfo, { pkg: project }))
+    .pipe(gulp.dest('dist'))
+    .pipe(minifyCSS())
+    .pipe(rename('gridy.min.css'))
+    .pipe(gulp.dest(gridPath.dist));
 });
 
 // sass and css
 gulp.task('css', function () {
-	gulp.src(path.src.scssFiles)
-		.pipe(plumber())
-		// compile sass, combine media queries, autoprefix and minify
-		.pipe(sass({sourceComments: null}))
-		.pipe(prefix({
-			browsers: ['last 2 versions', '> 1%'],
-			cascade: false
-		}))
-		.pipe(cssImgSize(path.imgPath))
-		.pipe(gulpif(!isDev, cssBase64({
-			baseDir: path.imgPath,
-			maxWeightResource: 10000,
-			extensionsAllowed: ['.gif', '.jpg', '.png']
-		})))
-		.pipe(gulpif(!isDev, minifyCSS()))
-		.pipe(header(projectInfo, {pkg: project}))
-		.pipe(gulp.dest(path.dist.css));
+  gulp.src(path.src.scssFiles)
+    .pipe(plumber())
+    // compile sass, combine media queries, autoprefix and minify
+    .pipe(sass({ sourceComments: null }))
+    .pipe(prefix({
+      browsers: [ 'last 2 versions', '> 1%' ],
+      cascade: false
+    }))
+    .pipe(cssImgSize(path.imgPath))
+    .pipe(gulpif(!isDev, cssBase64({
+      baseDir: path.imgPath,
+      maxWeightResource: 10000,
+      extensionsAllowed: [ '.gif', '.jpg', '.png' ]
+    })))
+    .pipe(gulpif(!isDev, minifyCSS()))
+    .pipe(header(projectInfo, { pkg: project }))
+    .pipe(gulp.dest(path.dist.css));
 });
 
 // scripts
 gulp.task('js', function () {
-	// detect errors in custom.js
-	gulp.src(path.src.js + 'custom.js')
-		.pipe(plumber())
-		.pipe(gulpif(isDev, jshint()))
-		.pipe(gulpif(isDev, jshint.reporter(stylish)))
-	// concatenate and minify files
-	gulp.src([
-		path.src.js + 'plugins/*.js',
-		path.src.js + 'custom.js'
-	])
-		.pipe(plumber())
-		.pipe(changed(path.dist.js))
-		.pipe(concat('./scripts.js'))
-		.pipe(gulpif(!isDev, uglify({preserveComments: 'some'})))
-		.pipe(header(projectInfo, {pkg: project}))
-		.pipe(gulp.dest(path.dist.js));
-	// move vendors and polyfill files in the dist folder
-	gulp.src([
-		path.src.js + 'vendor/*.js',
-		path.src.js + 'polyfill/*.js'
-	])
-		.pipe(plumber())
-		.pipe(changed(path.dist.js))
-		.pipe(gulp.dest(path.dist.js));
+  // detect errors in custom.js
+  gulp.src(path.src.js + 'custom.js')
+    .pipe(plumber())
+    .pipe(gulpif(isDev, jshint()))
+    .pipe(gulpif(isDev, jshint.reporter(stylish)))
+  // concatenate and minify files
+  gulp.src([
+    path.src.js + 'plugins/*.js',
+    path.src.js + 'custom.js'
+  ])
+    .pipe(plumber())
+    .pipe(changed(path.dist.js))
+    .pipe(concat('./scripts.js'))
+    .pipe(gulpif(!isDev, uglify({ preserveComments: 'some' })))
+    .pipe(header(projectInfo, { pkg: project }))
+    .pipe(gulp.dest(path.dist.js));
+  // move vendors and polyfill files in the dist folder
+  gulp.src([
+    path.src.js + 'vendor/*.js',
+    path.src.js + 'polyfill/*.js'
+  ])
+    .pipe(plumber())
+    .pipe(changed(path.dist.js))
+    .pipe(gulp.dest(path.dist.js));
 });
 
 // fonts
 gulp.task('font', function () {
-	gulp.src(path.src.fontFiles)
-		.pipe(plumber())
-		.pipe(changed(path.dist.font))
-		.pipe(gulp.dest(path.dist.font));
+  gulp.src(path.src.fontFiles)
+    .pipe(plumber())
+    .pipe(changed(path.dist.font))
+    .pipe(gulp.dest(path.dist.font));
 });
 
 // imgs
 gulp.task('img', function () {
-	gulp.src([path.src.imgFiles, '!' + path.src.img + 'test/**/*' + imgExt]) // exclude test path
-		.pipe(plumber())
-		.pipe(changed(path.dist.img))
-		.pipe(gulp.dest(path.dist.img));
+  gulp.src([ path.src.imgFiles, '!' + path.src.img + 'test/**/*' + imgExt ]) // exclude test path
+    .pipe(plumber())
+    .pipe(changed(path.dist.img))
+    .pipe(gulp.dest(path.dist.img));
 });
 
 // json package editor
 gulp.task('json', function () {
-	gulp.src(path.json)
-		.pipe(plumber())
-		.pipe(jeditor({
-			'version': project.version, // make sure this is a supported npm version
-			'name': project.shortname,
-			'description': project.descr,
-			'author': project.author
-		}))
-		.pipe(gulp.dest('./'));
+  gulp.src(path.json)
+    .pipe(plumber())
+    .pipe(jeditor({
+      'version': project.version, // make sure this is a supported npm version
+      'name': project.shortname,
+      'description': project.descr,
+      'author': project.author
+    }))
+    .pipe(gulp.dest('./'));
 });
 
 // watch files
 gulp.task('watch', function () {
-	gulp.watch(path.src.gridscssFiles, ['cssGrid']);
-	gulp.watch(path.src.scssFiles, ['css']);
-	gulp.watch(path.src.jsFiles, ['js']);
-	gulp.watch(path.src.fontFiles, ['font']);
-	gulp.watch(path.src.imgFiles, ['img']);
+  gulp.watch(path.src.gridscssFiles, [ 'cssGrid' ]);
+  gulp.watch(path.src.scssFiles, [ 'css' ]);
+  gulp.watch(path.src.jsFiles, [ 'js' ]);
+  gulp.watch(path.src.fontFiles, [ 'font' ]);
+  gulp.watch(path.src.imgFiles, [ 'img' ]);
 });
 
 // default task
-gulp.task('default', ['cssGrid', 'css', 'js', 'font', 'img', 'json', 'watch']);
+gulp.task('default', [ 'cssGrid', 'css', 'js', 'font', 'img', 'json', 'watch' ]);
